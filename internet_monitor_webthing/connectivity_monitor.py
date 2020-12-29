ab184207f91af1bb415400687ac6267b73e2eff3
Gregor Roth <gregor.roth@web.de>
2020-12-29 09:55:44 +0100

    cert check activated

--- internet_monitor_webthing/connectivity_monitor.py
+++ internet_monitor_webthing/connectivity_monitor.py
@@ -114,7 +114,7 @@ class IpInfo:
                 self.cache = dict()
                 self.cached_invalidated_time = datetime.now()
             if ip not in self.cache.keys():
-                response = requests.get('https://tools.keycdn.com/geo.json?host=' + ip, verify=False)
+                response = requests.get('https://tools.keycdn.com/geo.json?host=' + ip)
                 if (response.status_code >= 200) and (response.status_code < 300):
                     data = response.json()
                     self.cache[ip] = data['data']['geo']['isp']
