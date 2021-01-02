1ed68f7483e8f203d2941e96be3ae6dececbc21f
Gregor Roth <gregor.roth@web.de>
2021-01-02 08:52:18 +0100

    hosts handling workaround

--- internet_monitor_webthing/internet_multiple_webthing.py
+++ internet_monitor_webthing/internet_multiple_webthing.py
@@ -3,19 +3,36 @@ from webthing.utils import get_addresses
 from internet_monitor_webthing.connectivity_monitor_webthing import InternetConnectivityMonitorWebthing
 from internet_monitor_webthing.speedtest_monitor_webthing import InternetSpeedMonitorWebthing
 from webthing import (MultipleThings, WebThingServer)
-from threading import Timer
+import threading
 import logging
+import time
 
 
 
-def update_hosts(hosts: List[str], port : int):
-    for address in get_addresses():
-        if address not in hosts:
-            hosts.extend([
-                address,
-                '{}:{}'.format(address, port),
-            ])
-            logging.info("hosts extended with " + address)
+class UpdateHosts:
+
+    def __init__(self, hosts: List[str], port : int):
+        self.hosts = hosts
+        self.port = port
+        self.running = True
+
+    def start(self):
+        threading.Thread(target=self.run).start()
+
+    def run(self):
+        time.sleep(60)
+        while self.running:
+            for address in get_addresses():
+                if address not in self.hosts:
+                    self.hosts.extend([
+                        address,
+                        '{}:{}'.format(address, self.port),
+                    ])
+                    logging.info("hosts extended with " + address)
+            time.sleep(3 * 60 * 60)
+
+    def cancel(self):
+        self.running = False
 
 
 def run_server(hostname: str, port: int, description: str, speedtest_period: int, connecttest_period: int, connecttest_url: str):
@@ -33,7 +50,7 @@ def run_server(hostname: str, port: int, description: str, speedtest_period: int
         else:
             server = WebThingServer(MultipleThings(services, "Internet Monitor"), port=port)
 
-        hosts_updater = Timer(10 * 60, update_hosts, [server.hosts, port])
+        hosts_updater = UpdateHosts(server.hosts, port)
         try:
             logging.info('starting the server')
             logging.info("supported host headers: " + str(server.hosts))
