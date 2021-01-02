5abf4f388576122a31d9cab89cf7cbc55b99d4b9
Gregor Roth <gregor.roth@web.de>
2021-01-02 08:43:04 +0100

    hosts update added

--- internet_monitor_webthing/internet_multiple_webthing.py
+++ internet_monitor_webthing/internet_multiple_webthing.py
@@ -1,8 +1,21 @@
 from typing import List
+from webthing.utils import get_addresses
 from internet_monitor_webthing.connectivity_monitor_webthing import InternetConnectivityMonitorWebthing
 from internet_monitor_webthing.speedtest_monitor_webthing import InternetSpeedMonitorWebthing
 from webthing import (MultipleThings, WebThingServer)
+from threading import Timer
 import logging
+
+
+
+def update_hosts(hosts: List[str], port : int):
+    for address in get_addresses():
+        if address not in hosts:
+            hosts.extend([
+                address,
+                '{}:{}'.format(address, port),
+            ])
+            logging.info("hosts extended with " + address)
 
 
 def run_server(hostname: str, port: int, description: str, speedtest_period: int, connecttest_period: int, connecttest_url: str):
@@ -19,12 +32,16 @@ def run_server(hostname: str, port: int, description: str, speedtest_period: int
             server = WebThingServer(MultipleThings(services, "Internet Monitor"), hostname=hostname, port=port)
         else:
             server = WebThingServer(MultipleThings(services, "Internet Monitor"), port=port)
+
+        hosts_updater = Timer(10 * 60, update_hosts, [server.hosts, port])
         try:
             logging.info('starting the server')
             logging.info("supported host headers: " + str(server.hosts))
+            hosts_updater.start()
             server.start()
         except KeyboardInterrupt:
             logging.info('stopping the server')
+            hosts_updater.cancel()
             server.stop()
             logging.info('done')
     else:
